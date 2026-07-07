e11a2e60affd2fea9f2048fd2314a7cfaed869f2
aashish91422062-bit <aashish91422062@gmail.com>
2026-07-07 09:40:41 +0530

    Add side menu toggle functionality
    Implemented side menu toggle functionality with click events.

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("Ashish AI Loaded Successfully");
+// Ashish AI Studio V4
+
+const menuBtn = document.getElementById("menuBtn");
+const sideMenu = document.getElementById("sideMenu");
+
+let menuOpen = false;
+
+menuBtn.addEventListener("click", () => {
+
+    if (!menuOpen) {
+
+        sideMenu.style.left = "0";
+
+        menuOpen = true;
+
+    } else {
+
+        sideMenu.style.left = "-260px";
+
+        menuOpen = false;
+
+    }
+
+});
+
+// Menu के बाहर क्लिक करने पर Menu बंद हो जाएगा
+
+document.addEventListener("click", function(e){
+
+    if(
+        menuOpen &&
+        !sideMenu.contains(e.target) &&
+        !menuBtn.contains(e.target)
+    ){
+
+        sideMenu.style.left="-260px";
+
+        menuOpen=false;
+
+    }
+
+});
